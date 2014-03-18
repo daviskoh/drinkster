b343e0a3f98e556be3013c02b6d0f2bcacedeb59
daviskoh <koh.davis.0@gmail.com>
2014-03-18 16:00:03 -0400

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Drinkster::Application.config.secret_key_base = '534b5c0067d99c70e22f827aa4ec9a3cf9e959baaf78ace06f3e0f03305851d470b9c5a8c14139ce28451cee60ab9c72b4f330db155089aeefc2424408c4ec4d'
