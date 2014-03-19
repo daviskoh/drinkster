948fb255fee0de4b1618ea540fc570d80994b013
daviskoh <koh.davis.0@gmail.com>
2014-03-18 20:12:19 -0400

    can make post request to /users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: secure_user.as_json, status: 200
+    else
+      render status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def secure_user
+    {id: @user.id, email: @user.email}
+  end
+end
