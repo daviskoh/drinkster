46a926556d6ff2d6474a4cb95d5b85a4b014e415
daviskoh <koh.davis.0@gmail.com>
2014-03-19 17:12:12 -0400

    prevents login w/ wrong credentials

--- spec/requests/api/session_api_spec.rb
+++ spec/requests/api/session_api_spec.rb
@@ -18,9 +18,19 @@ describe 'Session API' do
     { email: 'bob@bob.com', password: 'bob' }.to_json
   }
 
+  let(:wrong_user_params) { 
+    { email: 'bob@bob.com', password: 'b' }.to_json
+  }
+
   it 'allows you log in' do 
     post '/api/session', user_params, request_headers
 
     expect(response).to be_success
+  end
+
+  it 'prevents login using wrong credentials' do 
+    post '/api/session', wrong_user_params, request_headers
+
+     expect(response.status).to eq 400
   end
 end
