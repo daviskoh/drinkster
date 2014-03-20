44958e55a9ecb23c5b32f63aac85f58b163ccf81
daviskoh <koh.davis.0@gmail.com>
2014-03-20 00:40:18 -0400

    login redirects to user's page

--- app/assets/javascripts/controllers/user_controller.js
+++ app/assets/javascripts/controllers/user_controller.js
@@ -1,20 +1,23 @@
-Drinkster.controller('UserCtrl', ['$scope', '$http', '$location', function($scope, $http, $location) {
-  $scope.getData = function() {
-    return {
-      'user': {
-        'email': $scope.email,
-        'password': $scope.password,
-        'password_confirmation': $scope.passwordConfirmation
-      }
+Drinkster.controller('UserCtrl', ['$scope', '$http', '$location', '$rootScope',
+  function($scope, $http, $location, $rootScope) {
+    $scope.getData = function() {
+      return {
+        'user': {
+          'email': $scope.email,
+          'password': $scope.password,
+          'password_confirmation': $scope.passwordConfirmation
+        }
+      };
     };
-  };
 
-  $scope.createUser = function() {
-    $http.post('/api/users.json', $scope.getData()).success(function() {
-      console.log('post SUCCESS');
-      console.log(arguments);
+    $scope.createUser = function() {
+      $http.post('/api/users.json', $scope.getData()).success(function() {
+        console.log('post SUCCESS');
+        console.log(arguments);
 
-      $location.path('/session/new');
-    });
-  };
-}]);
+        $location.path('/session/new');
+      });
+    };
+
+    $scope.email = $rootScope.user;
+  }]);
--- app/assets/javascripts/services/auth.js
+++ app/assets/javascripts/services/auth.js
@@ -1,15 +1,17 @@
-Drinkster.factory('Auth', ['$http', '$location',
-  function($http, $location) {
+Drinkster.factory('Auth', ['$http', '$location', '$rootScope',
+  function($http, $location, $rootScope) {
     return {
       isLoggedIn: function() {
         return window.currentUser ? true : false;
       },
       logIn: function(userData) {
-        $http.post('/api/session.json', userData).success(function(resp) {
+        $http.post('/api/session.json', userData).success(function(resp, status, func, obj) {
           console.log('log in SUCCESS');
           console.log(arguments);
 
           window.currentUser = +resp;
+
+          $rootScope.user = obj.data.email;
 
           // redirect to current user's page
           $location.path('/users/' + resp);
