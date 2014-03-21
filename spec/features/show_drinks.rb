17c988476f3ca7893fa7f165af934d99845df222
daviskoh <koh.davis.0@gmail.com>
2014-03-21 19:17:32 -0400

    shows parts of each ingr of a drinK

--- spec/features/show_drinks.rb
+++ spec/features/show_drinks.rb
@@ -3,31 +3,48 @@ require 'spec_helper'
 describe 'show drinks', js: true do 
   context 'database is preseeded with makeable drinks' do 
     before :all do 
+      Ingredient.destroy_all
+      Drink.destroy_all
+      User.destroy_all
+
       @drink = FactoryGirl.create(:gin_tonic)
-      @gin = FactoryGirl.create(:ingredient2)
-      @tonic = FactoryGirl.create(:ingredient3)
 
       @user = FactoryGirl.create(:user)
+
+      [FactoryGirl.create(:ingredient2), FactoryGirl.create(:ingredient3)].each do |ing|
+        @drink.ingredients << ing
+        @user.ingredients << ing
+      end
+
+      @c1 = Component.first
+      @c1.parts = 3
+      @c1.save      
+
+      @c2 = Component.last
+      @c2.parts = 1
+      @c2.save
     end
 
     after :all do 
       Ingredient.destroy_all
+      Drink.destroy_all
+      Component.destroy_all
       User.destroy_all
     end
 
     before :each do 
-      @drink.ingredients << @gin
-      @drink.ingredients << @tonic
-
-      @user.ingredients << @gin
-      @user.ingredients << @tonic
-
       user_login
     end
 
     it 'shows what drinks a user can make' do 
       within 'ul.drinks' do
         expect(page).to have_content 'Gin and tonic'
+      end
+    end
+
+    it 'shows parts of each ingredient of a drink' do 
+      within 'ul.components' do 
+        expect(page).to have_content 'Parts: 3'
       end
     end
 
